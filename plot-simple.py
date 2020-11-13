c1e7615e3ce77ab44acd55872c6c9ddee3f06dd9
Scott Deakin <sdeaks@gmail.com>
2020-11-13 16:50:43 +0000

    add comments to the plot source

--- plot-simple.py
+++ plot-simple.py
@@ -4,7 +4,7 @@ import plot
 file1 = 'log/imola/ks_corvette_c7r/20201112T215006_Scott_Deakin/lap_2.txt'
 file2 = 'log/imola/ks_corvette_c7r/20201112T215006_Scott_Deakin/lap_0.txt'
 
-plot_width=800
+plot_width=1000
 plot_height=600
 
 plot.combined_charts(file1, file2, plot_width=plot_width, plot_height=plot_height)
--- plot.py
+++ plot.py
@@ -5,8 +5,6 @@ from bokeh.models import ColumnDataSource, RangeTool, LinearAxis, Range1d, Title
 import csv
 import argparse
 
-
-
 def load_lap(file):
 
     data = {}
@@ -31,7 +29,7 @@ def invert_data(data):
     return list(map( lambda x: -x, data))
 
 
-def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_height=600):
+def combined_charts(file1, file2, output='plot.html', plot_width=1000, plot_height=600):
 
     data1 = load_lap(file1)
     data2 = load_lap(file2)
@@ -39,7 +37,7 @@ def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_heigh
     dlen1 = len(data1['distance'])
     dlen2 = len(data2['distance'])
 
-    # prepare some data
+    # prepare some padded data
     padto = max(dlen1, dlen2)
 
     delta = []
@@ -64,7 +62,7 @@ def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_heigh
     range_start=int(padto * 0.25)
     range_end=int(padto * 0.5)
 
-    # some empty data for the track
+    # some initial data for the track, using the range determined above
     track_source = ColumnDataSource(data=dict(
         x1=data['x1'][range_start:range_end], 
         z1=data['z1'][range_start:range_end], 
@@ -72,6 +70,7 @@ def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_heigh
         z2=data['z2'][range_start:range_end]
     ))
 
+    # we want to use the distance of the longest list
     if dlen1 < dlen2:
         data['distance'] = data2['distance']
     else:
@@ -87,9 +86,9 @@ def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_heigh
         ("value", "$y")
     ]
 
-    # create a new plot with a title and axis labels
+    # main plot showing the telemetry
     p = figure(
-        plot_height=plot_height, plot_width=plot_width,
+        plot_height=plot_height, plot_width=int(plot_width * .6),
         toolbar_location=None,
         tools='xpan, hover',
         tooltips=TOOLTIPS, x_axis_label='distance', y_axis_label='mph',
@@ -99,27 +98,30 @@ def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_heigh
     p.add_layout(Title(text="(1) " + file1, text_font_style="italic"), 'above')
     p.add_layout(Title(text="AC Telemetry", text_font_size="16pt"), 'above')
 
+    # zoom plot used to narrow down the other plots
+    # shows the delta from lap1 to lap2
     select = figure(title="Drag the middle and edges of the selection box to change the range above",
                     plot_height=130, plot_width=plot_width,
                     x_axis_type=None, y_axis_label='delta (s)',
                     tools="", toolbar_location=None, background_fill_color="#f9f9f9")
 
-
-    track = figure(title='Track', plot_width=plot_height, plot_height=plot_height, 
+    # track plot
+    # uses a dynamically created set of data, essentially the list of x,z that 
+    # would be visible with the range currently set
+    track = figure(title='Track', plot_width=int(plot_width * .4), plot_height=plot_height, 
         tools='', toolbar_location=None, x_axis_label='meters', y_axis_label='meters')
     track.line('x1', 'z1', source=track_source, line_width=2, muted_alpha=0.2, legend_label='lap1')
     track.line('x2', 'z2', source=track_source, line_width=2, muted_alpha=0.2, legend_label='lap2', line_dash='dashed')
     track.legend.location = "top_left"
     track.legend.click_policy="mute"
+    # make sure the units are kept the same on x,z
     track.match_aspect = True
 
-    #mph1_max = max(data['mph1'])
-    #mph2_max = max(data['mph2'])
-    #p.y_range = Range1d(start=0, end=max(mph1_max, mph2_max) )
+    # create a new range for the pedals and assign the scale to the right
     p.extra_y_ranges['pedal'] = Range1d(start=0, end=1.01)
     p.add_layout(LinearAxis(y_range_name='pedal', axis_label='pedal'), 'right')
 
-    # add a line renderer with legend and line thickness
+    # add all the telemetry lines
     p.line('distance', 'mph1', muted_alpha=0.2, source=source, legend_label="mph1", line_width=2)
     p.line('distance', 'mph2', muted_alpha=0.2, source=source, legend_label="mph2", line_width=2, line_dash='dashed')
     p.line('distance', 'gas1', muted_alpha=0.2, source=source, color='green', legend_label="gas1", line_width=2, y_range_name = 'pedal')
@@ -130,13 +132,13 @@ def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_heigh
     p.legend.location = "top_left"
     p.legend.click_policy="mute"
 
+    # this is the little block that shows the current range
     range_tool = RangeTool(x_range=p.x_range)
     range_tool.overlay.fill_color = "navy"
     range_tool.overlay.fill_alpha = .2
 
-    #select.y_range = Range1d(start=min(data['delta']), end=max(data['delta']) )
+    # the range selection uses a plot of the delta
     select.line('distance', 'delta', source=source)
-    #select.ygrid.grid_line_color = None
     select.add_tools(range_tool)
     select.toolbar.active_multi = range_tool
 
@@ -157,8 +159,18 @@ def combined_charts(file1, file2, output='plot.html', plot_width=800, plot_heigh
     p.x_range.js_on_change('start', trk_update) 
     p.x_range.js_on_change('end', trk_update) 
 
+    # allow dynamic sizing
+    p.sizing_mode = 'stretch_width'
+    #p.width_policy = 'max'
+    #track.sizing_mode = 'stretch_width'
+    #track.width_policy = 'min'
+    select.sizing_mode = 'stretch_width'
     # display the charts
-    show(column(row(p, track), select))
+    row1 = row(p, track)
+    row1.sizing_mode = 'stretch_width'
+    col = column(row1, select)
+    col.sizing_mode = 'stretch_width'
+    show(col)
 
 
 if __name__ == '__main__':
